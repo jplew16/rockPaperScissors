7d6f489a0b4c2e3e496f1d09180e9846f7567d5b
Jonathan Plewinski <j.plewinski@yahoo.com>
2021-04-10 12:31:09 -0600

    Created play a single round, play five rounds with score tracking, and announce the game winner functions :)

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-function computerPlay (cpuRoll) {
-    let cpuChoice = Math.floor(Math.random() * cpuRoll);
-     (cpuChoice == 0) ? cpuChoice = "rock" :
-     (cpuChoice == 1) ? cpuChoice = "scissors" :
-     cpuChoice = "paper";
-     return cpuChoice;
-}
-computerPlay(3) // 0 is rock, 1 is scissors, 2 is paper
+function computerPlay (computerRoll = 3) {
+     let randomNum = Math.floor(Math.random() * computerRoll);
+     (randomNum == 0) ? computerRoll = "rock" :
+     (randomNum == 1) ? computerRoll = "scissors" :
+     computerRoll = "paper";
+     return computerRoll;
+} 
 
-function playRound(playerChoice, cpuChoice) {
-    (playerChoice == cpuChoice) ? "ties with" :
-    (playerChoice == "paper" && cpuChoice == "rock") ? "wins over" :
-    (playerChoice == "rock" && cpuChoice == "paper") ? "loses to" :
-    (playerChoice == "scissors" && cpuChoice == "paper") ?  "wins over" :
-    (playerChoice == "paper" && cpuChoice == "scissors") ? "loses to" :
-    "Please enter rock, paper, or scissors";
-    
+
+function keepScore(roundOver, computerScore, playerScore) {
+    if (roundOver == "wins over") {
+        playerScore++ 
+        return playerScore;
+    } else if (roundOver == "loses to") {
+        computerScore++;
+        return computerScore;
+    }
 }
+function playRound(playerChoice, computerChoice) {
+    let roundOver = (playerChoice == computerChoice) ? "ties with" :
+    (playerChoice == "paper" && computerChoice == "rock") ? "wins over"  :
+    (playerChoice == "rock" && computerChoice == "scissors") ? "wins over"  :
+    (playerChoice == "scissors" && computerChoice == "rock") ? "loses to"  :
+    (playerChoice == "rock" && computerChoice == "paper") ? "loses to" :
+    (playerChoice == "scissors" && computerChoice == "paper") ?  "wins over" :
+    (playerChoice == "paper" && computerChoice == "scissors") ? "loses to" :
+    "Either rock, paper, or scissors was not entered";
+    
+    console.log(`${playerChoice} ${roundOver} ${computerChoice}`)
+    return roundOver;
+}
+
+function game(playerScore = 0, computerScore = 0) {
+    for (i = 1; i <= 5; i++) {
+        let roundWinner = playRound(
+            prompt(`Round ${i}: Please type: Rock, Paper, or Scissors?`).toLowerCase(),
+            computerPlay(),
+        )
+        if (roundWinner == "wins over") { 
+            playerScore++ 
+        }
+        else if (roundWinner != "ties with") { 
+            computerScore++ 
+        }
+        console.log(`Player Score : ${playerScore}
+Computer Score: ${computerScore}`)
+
+    }
+    gameOver(playerScore, computerScore)
+}
+
+function gameOver(playerScore, computerScore)  {
+    (playerScore > computerScore) ? console.log("Congratulations! You Win! :)") :
+    (playerScore < computerScore) ? console.log("You Lose! Nice Try!") :
+    console.log("Fair Game, It's a Tie!");
+}
+game()
